62b0ec2c696e815795ef7714a272c2d37133bd6a
Nick Krichevsky <njk828@gmail.com>
2015-02-09 20:37:53 -0500

    Correct Font Awesome errors and account for issues with height issues on OSX

--- popup.js
+++ popup.js
@@ -1,44 +1,63 @@
-function getWindows(windowList){
+function getWindows(windowList,callback){
 	chrome.windows.getAll(function(windows){
 		//loop through every window and append it ot the list
 		windows.forEach(function(currentWindow,i){
-			var li = document.createElement("li");
-			var ul = document.createElement("ul");
-			chrome.tabs.query({'windowId':currentWindow.id},function(tabs){
+			getTabs(currentWindow.id,function(tabs){
+				var li = document.createElement("li");
+				var ul = document.createElement("ul");
 				li.classList.add("window");
-				li.textContent="Window "+(i+1)+" - "+tabs.length+" tabs";
+				li.textContent="Window "+(i+1)+" - "+tabs.length+ (tabs.length>1 ? " tabs":" tab");
+				tabs.forEach(function(currentTab){
+					ul.appendChild(currentTab);
+				});
 				li.appendChild(ul);
 				windowList.appendChild(li);
-				tabs.forEach(function(currentTab){
-					var li = document.createElement("li");
-					var closeButton = document.createElement("i");
-					closeButton.classList.add("fa");
-					closeButton.classList.add("fa-remove");
-					closeButton.classList.add("close");
-					closeButton.classList.add("noselect");
-					closeButton.onclick = function(event){
-						event.preventDefault();
-						event.stopPropagation();
-						chrome.tabs.remove(currentTab.id);
-						ul.removeChild(li);
-					}
-					li.classList.add("tab");
-					li.textContent=currentTab.title;
-					li.appendChild(closeButton);
-					ul.appendChild(li);
-				});
+				callback(li);
 			});
 		});
 	});
 
 }
 
+function getTabs(windowId,callback){
+	var windowTabs = []
+	chrome.tabs.query({'windowId':windowId},function(tabs){
+		tabs.forEach(function(currentTab){
+			var li = document.createElement("li");
+			var closeButton = document.createElement("i");
+			closeButton.classList.add("fa");
+			closeButton.classList.add("fa-remove");
+			closeButton.classList.add("close");
+			closeButton.classList.add("noselect");
+			closeButton.onclick = function(event){
+				event.preventDefault();
+				event.stopPropagation();
+				chrome.tabs.remove(currentTab.id);
+				ul.removeChild(li);
+			}
+			li.classList.add("tab");
+			li.textContent=currentTab.title;
+			li.appendChild(closeButton);
+			windowTabs.push(li);
+		});
+		callback(windowTabs);
+	});
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	var mainList = document.getElementById("windows");
 	var body = document.getElementsByTagName("body")[0];
-	getWindows(mainList);
-	if (mainList.clientHeight<600){
-		body.style.height = mainList.clientHeight;
-	}
+	var html = document.getElementsByTagName("html")[0];
+	var totalHeight = 0;
+	getWindows(mainList,function(tabs){
+		totalHeight+=tabs.clientHeight;
+		var height = 600+"px";
+		if (totalHeight<600){
+			height = totalHeight+"px";
+		}
+		html.style.height = height;
+		body.style.height = height;
+	});
+	
 	
 });
