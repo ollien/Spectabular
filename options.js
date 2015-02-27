d30e817c5ec999bb9995c9abc8fc27b1167b223e
Nick Krichevsky <njk828@gmail.com>
2015-02-27 11:11:08 -0500

    Add setting up of view based on options

--- options.js
+++ options.js
@@ -14,18 +14,27 @@ function saveOptions(callback){
 	chrome.storage.local.set({"options":options},callback);
 }
 
+function setupOptionsView(optionsDiv){
+	var children = Array.prototype.slice.call(optionsDiv.querySelectorAll('input.option'));
+	children.forEach(function(child){
+		child.checked = options[child.id];
+	});
+}
+
 document.addEventListener('DOMContentLoaded', function(event){
 	var optionsDiv = document.getElementById("options");
 	getOptions(function(data){
 		if (Object.keys(data).length===0){
 			createOptionsStorage(function(){
 				getOptions(function(data){
-					options = data;
+					options = data.options;
+					setupOptionsView(optionsDiv);
 				});
 			});
 		}
 		else{
-			options = data;
+			options = data.options;
+			setupOptionsView(optionsDiv);
 		}
 	});
 });
