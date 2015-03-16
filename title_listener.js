0e877264974acd3e6b274d1c67527dbe19449414
Nick Krichevsky <njk828@gmail.com>
2015-03-16 11:13:37 -0400

    Fix typo with MutationObserver length

--- title_listener.js
+++ title_listener.js
@@ -2,7 +2,7 @@ var title = document.querySelector("title");
 if (title!=null){
 	var observer = new MutationObserver(function(mutations){
 		mutations.forEach(function(mutation){
-			if (mutation.addedNodes.length===0 && mutation.removedNodes.length===0){
+			if (mutation.addedNodes.length===1 && mutation.removedNodes.length===1){
 				var newTitle = mutation.addedNodes[0].nodeValue;
 				var oldTitle = mutation.removedNodes[0].nodeValue;
 				chrome.runtime.sendMessage({"titleChange":{"newTitle":newTitle,"oldTitle":oldTitle}});
